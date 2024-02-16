7d771e8f20a9572d381e89984e8f7f3bce8f0963
Florent Giov annone <giovannoneflorent@gmail.com>
2024-02-16 10:38:12 +0000

    initial commit

---
+++ scripts/app.js
@@ -0,0 +1,48 @@
+function init() {
+const body = document.querySelector()
+    body.textContent = "hello";
+
+
+
+    // -----------PLAYER-----------
+        // Players character array
+    // Have the player spawn on the screen at the begining "add player function" adding classes
+    // once the player is hit the player loose a life (5 lifes total)
+    // Player removed once no more lifes "Remove player function" removing classes
+    // Have the player shoot and travel spawn only when space is pressed
+    // have the shoot from player disapeared once it hit an alien or the end of the grid
+
+
+
+
+
+    // -----------ALIENS-----------
+    // Array for "aliens"
+    // Have the aliens spawn on the screen "add alien function"  adding classes
+    // Have the alien disapear once hit "Remove alien function" removing classes
+    // Have the alien shoot and travel spawn
+    // have the shoot from alien disapeared once it hit the player or the end of the grid
+    // Array of starting positions of the aliens
+
+
+
+
+
+    // -----------MAIN-----------
+    // Create Score, Lives, Current level and highscore
+    // Create grid and rules for the player and the alien to stay withing the grid
+    // Make the game faster when the level id increased
+    // High score will have to be retrived from local memory
+    // Remove every class from the grid once the game is over
+    // create a seamless UI to enter and exit the game
+    // Have function for every stage of the game steps (welcome, play, end -> play again)
+    // have a function for each  level playable link to a button when the player is ready
+    // have a mute       
+    // incorporate assets via JS or CSS to keep HTML as clean as possible
+
+
+
+
+
+}
+window.addEventListener("DOMContentLoaded", init);
